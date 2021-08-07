099f0c707708cb190d0797c6720c466fd82d0363
38takumi <saitout485@gmail.com>
2021-08-07 14:09:51 +0000

    コード入力後

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,40 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  #followerについて
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 被フォロー関係を通じて参照→followed_idをフォローしている人
+  #followerについてここまで
+
+  #followingについて
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 【class_name: "Relationship"】は省略可能
+  has_many :followings, through: :relationships, source: :followed
+  # 与フォロー関係を通じて参照→follower_idをフォローしている人
+  #followingについてここまで
+
+
+def follow(user_id)
+  relationships.create(followed_id: user_id)
+end
+
+def unfollow(user_id)
+  relationships.find_by(followed_id: user_id).destroy
+end
+
+def following?(user)
+  followings.include?(user)
+end
+
+
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :name, length: { in: 2..20 }
 
   validates :introduction, length: { maximum: 50 }
 
-#プロフィール画像用 deviseに入ってるかも
+  #プロフィール画像用
   attachment :profile_image
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_135149) do
+ActiveRecord::Schema.define(version: 2021_08_07_132958) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "user_comment"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2021_08_05_135149) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
